32afc5d6ee68b2800c618b3dad8997c55a3133da
木ノ上　アイン <30123@ojs.ac.jp>
2020-10-01 11:56:56 +0900

    演習問題4.1

--- Chapter4_/Program.cs
+++ Chapter4_/Program.cs
@@ -11,31 +11,8 @@ namespace Chapter4_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetProduct());
-        }
 
-        private static string GetProduct()
-        {
-            Sale sale = new Sale
-            {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
-            };
-            sale = null;
-            return sale?.Product;
         }
-
     }
 
-    class Sale
-    {
-        //店舗名
-        public string ShopName { get; set; }
-        //売上高
-        public int Amount { get; set; } = 100000;
-        public string Product { get => product; set => product = value; }
-
-        private string product;
-    }
 }
---
+++ Chapter4_/YearMonth.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_
+{
+    class YearMonth
+    {
+        //4.1.1 プロパティ２つ、コンストラクタ
+        //年
+        public int Year { get; private set; }
+        //月
+        public int Month { get; private set; }
+
+        public YearMonth(int year,int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        //4.1.2 Is21Centuryプロパティを追加
+        public bool Is21Century { get { return 2001 <= Year && Year <= 2100; } }
+
+        //4.1.3 AddOneMonth()メソッドを追加
+        public YearMonth AddOneMonth()
+        {
+            if (Month == 12)
+            {
+                return new YearMonth(this.Year + 1, 1);
+            }
+            else
+            {
+                return new YearMonth(this.Year, this.Month + 1);
+            }
+        }
+
+        //4.1.4 ToString()メソッドのオーバーライド
+        public override string ToString()
+        {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
