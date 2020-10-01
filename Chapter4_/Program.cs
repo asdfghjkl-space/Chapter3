fc3f6558a7c3147a6f85d39e59486c5bc97459ba
木ノ上　アイン <30123@ojs.ac.jp>
2020-10-01 10:20:43 +0900

    P116

---
+++ Chapter4_/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = "12345";
+
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+        }
+
+        //スタブ
+        private static object DefaultMessage()
+        {
+            return "DefaultMessage";
+        }
+
+        //スタブ
+        private static object GetMessage(string code)
+        {
+            return 123;
+        }
+    }
+}
