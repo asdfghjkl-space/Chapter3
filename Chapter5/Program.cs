9e93c3aa449d20323392df636d6de2f55bf0228f
木ノ上　アイン <30123@ojs.ac.jp>
2020-10-02 15:54:12 +0900

    5-2

--- Chapter5/Program.cs
+++ Chapter5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,20 +12,31 @@ namespace Chapter5
         static void Main(string[] args)
         {
             //5-1
-            var mozi = Console.ReadLine();
-            var mozi2 = Console.ReadLine();
+            //var mozi = Console.ReadLine();
+            //var mozi2 = Console.ReadLine();
 
-            if(String.Compare(mozi,mozi2,true) == 0)
+            //if(String.Compare(mozi,mozi2,true) == 0)
+            //{
+            //    Console.WriteLine("等しい");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("等しくない");
+            //}
+
+            //5-2
+            var suuzi = Console.ReadLine();
+
+            int num;    //変換後の数値格納
+            if(int.TryParse(suuzi, out num))
             {
-                Console.WriteLine("等しい");
+                Console.WriteLine(num.ToString("#,#"));
             }
             else
             {
-                Console.WriteLine("等しくない");
+                Console.WriteLine("数値文字列ではありません。");
             }
-
-            //5-2
-
+            
         }
     }
 }
