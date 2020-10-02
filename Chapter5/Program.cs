72bc05341bf7ae077b9391eff343f472eb3a2991
木ノ上　アイン <30123@ojs.ac.jp>
2020-10-02 15:18:33 +0900

    5-1

--- Chapter5/Program.cs
+++ Chapter5/Program.cs
@@ -10,6 +10,21 @@ namespace Chapter5
     {
         static void Main(string[] args)
         {
+            //5-1
+            var mozi = Console.ReadLine();
+            var mozi2 = Console.ReadLine();
+
+            if(String.Compare(mozi,mozi2,true) == 0)
+            {
+                Console.WriteLine("等しい");
+            }
+            else
+            {
+                Console.WriteLine("等しくない");
+            }
+
+            //5-2
+
         }
     }
 }
