f081e921c465e6dea50c23b41576f4c9396634c0
木ノ上　アイン <30123@ojs.ac.jp>
2020-10-12 15:58:00 +0900

    7章　重複など

---
+++ Chapter7/MonthDay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7
+{
+    class MonthDay
+    {
+        public int Day { get; private set; }
+        public int Month { get; private set; }
+        public MonthDay(int month,int day)
+        {
+            this.Month = month;
+            this.Day = day;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as MonthDay;
+            if(other == null)
+               throw new ArgumentException();
+            return this.Day == other.Day && this.Month == other.Month;
+        }
+        //ハッシュコード
+        public override int GetHashCode()
+        {
+            var code = Month.GetHashCode();
+            return Month.GetHashCode() * 31 + Day.GetHashCode();
+        }
+    }
+}
--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Section01;
+using Section02;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +13,62 @@ namespace Chapter7
     {
         static void Main(string[] args)
         {
-            var flowerDict = new Dictionary<string, int>()
-            {
-                ["sunflower"] = 400,
-                ["pansy"] = 300,
-                ["tulip"] = 350,
-                ["rose"] = 500,
-                ["dahlia"] = 450,
-            };
+            #region
+            //var employeeDict = new Dictionary<int,Employee>()
+            //{
+            //    {100,new Employee(100,"清水？？") },
+            //    {112,new Employee(112,"？？？？") },
+            //    {125,new Employee(125,"岩瀬？？？") },
+            //};
 
-            flowerDict["violet"] = 600;
+            ////Console.WriteLine($"{employeeDict.Sum(s => s.Value.Id)}");
 
-            foreach (var item in flowerDict)
+            //var employees = employeeDict.Where(emp => emp.Value.Id % 2 == 0);
+
+            //foreach (/*KeyValuePair<int,Employee> → varがない場合は左尾用に書く*/var item in employeeDict)
+            //{
+            //    Console.WriteLine($"{item.Value.Id} = {item.Value.Name}");
+            //}
+
+            //var employee = new List<Employee>()
+            //{
+            //    new Employee(100,"清水？？"),
+            //    new Employee(112,"？？？？"),
+            //    new Employee(125,"岩瀬？？？"),
+            //    new Employee(143,"山田太郎"),
+            //    new Employee(148,"池田次郎"),
+            //    new Employee(152,"高田三郎"),
+            //    new Employee(155,"石川幸也"),
+            //    new Employee(161,"中沢信也"),
+            //};
+
+            ////IDが偶数のみディクショナリに変換する
+            //var employeeDict = employee.ToDictionary(emp => emp.Id);
+
+            //foreach (var item in employeeDict)
+            //{
+            //    Console.WriteLine($"{item.Value.Id} {item.Value.Name}");
+            //}
+            #endregion
+
+            var lines = File.ReadAllLines("sample.txt");
+            var we = new WordsExtractor(lines);
+
+            foreach (var word in we.Extract())
             {
-                Console.WriteLine($"{item.Key} = {item.Value}");
+                Console.WriteLine(word);
             }
+
+            //var dict = new Dictionary<MonthDay, string>
+            //{
+            //    {new MonthDay(3,5),"？？の日"},
+            //    {new MonthDay(8,4),"？の日"},
+            //    {new MonthDay(10,3),"登山の日"},
+            //};
+
+            //var md = new MonthDay(8, 4);
+            //var s = dict[md];
+            //Console.WriteLine(s);
         }
     }
 }
