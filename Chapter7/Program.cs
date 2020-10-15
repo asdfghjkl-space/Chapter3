f528857445715cdec072437bea105d0b6051610e
木ノ上　アイン <30123@ojs.ac.jp>
2020-10-15 11:55:19 +0900

    課題完成

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -13,62 +13,123 @@ namespace Chapter7
     {
         static void Main(string[] args)
         {
-            #region
-            //var employeeDict = new Dictionary<int,Employee>()
-            //{
-            //    {100,new Employee(100,"清水？？") },
-            //    {112,new Employee(112,"？？？？") },
-            //    {125,new Employee(125,"岩瀬？？？") },
-            //};
+            Console.WriteLine("********************");
+            Console.WriteLine("*辞書登録プログラム*");
+            Console.WriteLine("********************");
 
-            ////Console.WriteLine($"{employeeDict.Sum(s => s.Value.Id)}");
+            var dict = new Dictionary<string, List<string>>();
 
-            //var employees = employeeDict.Where(emp => emp.Value.Id % 2 == 0);
+            Console.WriteLine("1.登録  2.内容を表示  3.終了");
 
-            //foreach (/*KeyValuePair<int,Employee> → varがない場合は左尾用に書く*/var item in employeeDict)
-            //{
-            //    Console.WriteLine($"{item.Value.Id} = {item.Value.Name}");
-            //}
-
-            //var employee = new List<Employee>()
-            //{
-            //    new Employee(100,"清水？？"),
-            //    new Employee(112,"？？？？"),
-            //    new Employee(125,"岩瀬？？？"),
-            //    new Employee(143,"山田太郎"),
-            //    new Employee(148,"池田次郎"),
-            //    new Employee(152,"高田三郎"),
-            //    new Employee(155,"石川幸也"),
-            //    new Employee(161,"中沢信也"),
-            //};
-
-            ////IDが偶数のみディクショナリに変換する
-            //var employeeDict = employee.ToDictionary(emp => emp.Id);
-
-            //foreach (var item in employeeDict)
-            //{
-            //    Console.WriteLine($"{item.Value.Id} {item.Value.Name}");
-            //}
-            #endregion
-
-            var lines = File.ReadAllLines("sample.txt");
-            var we = new WordsExtractor(lines);
-
-            foreach (var word in we.Extract())
+            while (true)
             {
-                Console.WriteLine(word);
+                var sel = int.Parse(Console.ReadLine());
+
+                if (sel == 1)
+                {
+                    Console.Write("KEYを入力:");
+                    var key = Console.ReadLine();
+
+                    Console.Write("VALUEを入力:");
+                    var value = Console.ReadLine();
+
+                    Console.WriteLine("登録しました！");
+
+                    if (dict.ContainsKey(key))
+                    {
+                        dict[key].Add(value);
+                    }
+                    else
+                    {
+                        dict[key] = new List<string> { value };
+                    }
+
+                }
+
+                if (sel == 2)
+                {
+                    foreach (var item in dict)
+                    {
+                        foreach (var term in item.Value)
+                        {
+                            Console.WriteLine("{0} : {1}", item.Key, term);
+                        }
+                    }
+                    break;
+                }
+
+                if (sel == 3)
+                {
+                    Console.WriteLine("終了");
+                    break;
+                }
+
             }
 
-            //var dict = new Dictionary<MonthDay, string>
-            //{
-            //    {new MonthDay(3,5),"？？の日"},
-            //    {new MonthDay(8,4),"？の日"},
-            //    {new MonthDay(10,3),"登山の日"},
-            //};
 
-            //var md = new MonthDay(8, 4);
-            //var s = dict[md];
-            //Console.WriteLine(s);
         }
+
+        //public void DuplicateKeySample()
+        //{
+        //    // ディクショナリの初期化
+        //    var dict = new Dictionary<string, List<string>>() {
+        //       { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+        //       { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+        //    };
+
+        //    // ディクショナリに追加
+        //    var key = "EC";
+        //    var value = "電子商取引";
+        //    if (dict.ContainsKey(key))
+        //    {
+        //        dict[key].Add(value);
+        //    }
+        //    else
+        //    {
+        //        dict[key] = new List<string> { value };
+        //    }
+
+        //    // ディクショナリの内容を列挙
+        //    foreach (var item in dict)
+        //    {
+        //        foreach (var term in item.value)
+        //        {
+        //            Console.WriteLine("{0} : {1}", item.key, term);
+        //        }
+        //    }
+
+        //public void DuplicateKeySample()
+        //{
+        //    // ディクショナリの初期化
+        //    var dict = new Dictionary<string, List<string>>();
+
+        //    // ディクショナリに追加
+        //    Console.Write("KEYを入力:");
+        //    var key = Console.ReadLine();
+
+        //    Console.Write("VALUEを入力:");
+        //    var value = Console.ReadLine();
+
+        //    Console.WriteLine("登録しました！");
+
+        //    if (dict.ContainsKey(key))
+        //    {
+        //        dict[key].Add(value);
+        //    }
+        //    else
+        //    {
+        //        dict[key] = new List<string> { value };
+        //    }
+
+        //    // ディクショナリの内容を列挙
+        //    foreach (var item in dict)
+        //    {
+        //        foreach (var term in item.Value)
+        //        {
+        //            Console.WriteLine("{0} : {1}", item.Key, term);
+        //        }
+        //    }
+
+        //}
     }
 }
